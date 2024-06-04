7a2a092aefa0f8426128dcabddb6ecc985e7af09
[mzhelskikh.y] <yam@pfsz.ru>
2024-06-04 09:35:38 +0700

    добавлены решения задач 1, 2 для урока 1

--- CySharpAdvancedCourse/Lesson1/L1Task1/Program.cs
+++ CySharpAdvancedCourse/Lesson1/L1Task1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace L1Task1
 {
@@ -13,6 +14,83 @@ namespace L1Task1
     {
         public static void Main(string[] args)
         {
+
+            var year = new Year();
+
+            var months = year.GetMonthsByAmountOfDays(31);
+            foreach (var m in months)
+            {
+                Console.WriteLine(m.Title);
+            }
+            
+            var month = year.GetMonthByOrdinal(1);
+            Console.WriteLine(month.Title);
+        }
+    }
+
+    internal class Year
+    {
+        public readonly Month[] _months;
+        
+        public List<Month> GetMonthsByAmountOfDays(int amountOfDays)
+        {
+            List<Month> months = new List<Month>();
+            
+            for (var i = 0; i < _months.Length; i++)
+            {
+                var month = _months[i];
+                if (month.AmountOfDays == amountOfDays)
+                {
+                    months.Add(month);
+                }
+            }
+            return months;
+        }
+        
+        public Month GetMonthByOrdinal(int ordinal)
+        {
+            Month month = null;
+            
+            if (ordinal >= 1 && ordinal <= 12)
+            {
+                month = _months[ordinal - 1];
+            }
+            return month;
+        }
+
+        public Year()
+        {
+            _months = new[]
+            {
+                new Month(1, 31, "Январь"),
+                new Month(2, 28, "Февраль"),
+                new Month(3, 31, "Март"),
+                new Month(4, 30, "Апрель"),
+                new Month(5, 31, "Май"),
+                new Month(6, 30, "Июнь"),
+                new Month(7, 31, "Июль"),
+                new Month(8, 31, "Август"),
+                new Month(9, 30, "Сентябрь"),
+                new Month(10, 31, "Октябрь"),
+                new Month(11, 30, "Ноябрь"),
+                new Month(12, 31, "Декабрь")
+            };
+        }
+    }
+
+    internal class Month
+    {
+        public string Title { get; private set; }
+        
+        public int Ordinal { get; private set; }
+        
+        public int AmountOfDays { get; private set; }
+
+        public Month(int ordinal, int amountOfDays, string title)
+        {
+            Ordinal = ordinal;
+            AmountOfDays = amountOfDays;
+            Title = title;
         }
     }
 }
--- CySharpAdvancedCourse/Lesson1/L1Task2/Program.cs
+++ CySharpAdvancedCourse/Lesson1/L1Task2/Program.cs
@@ -1,16 +1,174 @@
-﻿namespace L1Task2
+﻿using System;
+using System.Collections.Generic;
+
+namespace L1Task2
 {
     /*
     Задание 3
     Создайте коллекцию, которая бы по свой структуре напоминала «родовое дерево» (имя
     человека, год рождения), причем в нее можно добавлять/удалять нового родственника, есть
     возможность увидеть всех наследников выбранного человека, отобрать родственников по году
-    рождения. 
+    рождения.
      */
     internal class Program
     {
         public static void Main(string[] args)
         {
+            Node node1 = new Node(
+                description: new NodeDescription(
+                    name: "Елизавета II",
+                    birthYear: 1926
+                    )
+                );
+            
+            Node node2 = new Node(
+                description: new NodeDescription(
+                    name: "принц Филипп II",
+                    birthYear: 1921
+                )
+            );
+            
+            Node node3 = new Node(
+                description: new NodeDescription(
+                    name: "Карл III",
+                    birthYear: 1948
+                )
+            );
+            
+            Node node4 = new Node(
+                description: new NodeDescription(
+                    name: "Диана",
+                    birthYear: 1961
+                )
+            );
+            
+            Node node5 = new Node(
+                description: new NodeDescription(
+                    name: "Уильям Принц Уэльский",
+                    birthYear: 1982
+                )
+            );
+            
+            Node node6 = new Node(
+                description: new NodeDescription(
+                    name: "Принц Гарри",
+                    birthYear: 1984
+                )
+            );
+
+            FamilyTree familyTree = new FamilyTree();
+
+            familyTree.AddChildToParent(node1, node3);
+            familyTree.AddChildToParent(node2, node3);
+            
+            familyTree.AddChildToParent(node3, node5);
+            familyTree.AddChildToParent(node3, node6); 
+            familyTree.AddChildToParent(node4, node5);
+            familyTree.AddChildToParent(node4, node6);
+
+            familyTree.ShowChildrenTreeForNode(node1);
         }
     }
+
+    internal class FamilyTree
+    {
+
+        private readonly List<Node> _familyMembers = new List<Node>();
+
+        public void AddChildToParent(Node parent, Node child)
+        {
+            _familyMembers.Add(child);
+            child.AddParent(parent);
+            parent.AddChild(child);
+        }
+        
+        public void RemoveChildFromParent(Node parent, Node child)
+        {
+            _familyMembers.Remove(child);
+            child.DeleteParent(child);
+            parent.DeleteChild(child);
+        }
+        
+        public void ShowChildrenTreeForNode(Node node)
+        {
+            HashSet<Node> setOfChildren = new HashSet<Node>();
+
+            IterateChildren(node, setOfChildren);
+            
+            foreach (var child in setOfChildren)
+            {
+                Console.WriteLine(child.Description.Name);
+            }
+
+            return;
+
+            void IterateChildren(Node tnode, HashSet<Node> children)
+            {
+                if (tnode.Children.Count == 0) return;
+                
+                foreach (var child in tnode.Children)
+                {
+                    children.Add(child);
+                }
+                foreach (var child in tnode.Children)
+                {
+                    IterateChildren(child, children);
+                }
+            }
+        }
+    }
+
+    internal class Node
+    {
+
+        
+        public readonly List<Node> Parents;
+        
+        public readonly List<Node> Children;
+        
+        public readonly NodeDescription Description;
+
+        public Node(NodeDescription description)
+        {
+            Description = description;
+            Parents = new List<Node>();
+            Children = new List<Node>();
+        }
+
+        public void AddChild(Node child)
+        {
+            Children.Add(child);
+        }
+        
+        public void DeleteChild(Node child)
+        {
+            if (!Children.Remove(child)) throw new Exception("DeleteChild");
+        }
+        
+        public void AddParent(Node parent)
+        {
+            if (Parents.Count >= 2) throw new Exception("AddParent");
+            Parents.Add(parent);
+        }
+        
+        public void DeleteParent(Node parent)
+        {
+            if (!Parents.Remove(parent)) throw new Exception("DeleteParent");
+        }
+    }
+
+
+    internal class NodeDescription
+    {
+        public readonly string Name;
+        
+        public readonly int BirthYear;
+
+        public NodeDescription(string name, int birthYear)
+        {
+            Name = name;
+            BirthYear = birthYear;
+        }
+    }
+
 }
