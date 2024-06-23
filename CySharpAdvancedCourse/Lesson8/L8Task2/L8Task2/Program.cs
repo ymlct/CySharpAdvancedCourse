ab7c361d114e2ffce110c8ff2efdcc1c9f7c7fd3
[mzhelskikh.y] <yam@pfsz.ru>
2024-06-24 05:29:31 +0700

    добавлены решения задач для уроков 8, 9

---
+++ CySharpAdvancedCourse/Lesson8/L8Task2/L8Task2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Xml.Serialization;
+
+namespace L8Task2
+{
+    
+    /*
+    Задание 3 
+    Создайте новое приложение, в котором выполните десериализацию объекта из предыдущего 
+    примера. Отобразите состояние объекта на экране. 
+    */
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(SerializableClass));
+
+            using (FileStream source = new FileStream("/Lesson8/L8Task2/L8Task2/SerializableClass.xml", FileMode.Open, FileAccess.Read))
+            {
+                SerializableClass serializableClass = (SerializableClass )serializer.Deserialize(source);
+                
+            }
+        }
+    }
+    
+    [XmlRoot("CustomRootName")]     
+    public class SerializableClass
+    {
+
+        // [XmlAttribute("StringFieldAsXmlAttribute")]
+        public string StringField;
+        
+        
+        public double DoubleField;
+        
+        
+        public List<int> ListOfInts;
+        
+        
+        public ChildSerializableClass CustomTypeField;
+
+    }
+    
+    
+    public class ChildSerializableClass
+    {
+        public bool BoolField;
+    }
+}
