74b00decd22bc4126ba167b1535e2f4c1253477e
[mzhelskikh.y] <yam@pfsz.ru>
2024-06-29 16:07:24 +0700

    добавлены решения задач к урокам 10, 11

---
+++ CySharpAdvancedCourse/Lesson10/L10Task1/Program.cs
@@ -0,0 +1,78 @@
+﻿namespace L10Task1
+{
+    
+    /*
+    Напишите небольшую программу на языке C#, представляющую собой абстрактную 
+    реализацию шаблона Template method (Шаблонный метод). 
+    */
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            TemplateMethodDerived clazz = new TemplateMethodDerived();
+            
+            clazz.MainAction();
+        }
+    }
+    
+    internal class TemplateMethodBase
+    {
+
+        internal void MainAction()
+        {
+            InitialAction();
+            SubAction1();
+            SubAction2();
+            SubAction3();
+            FinalAction();
+        }
+        
+        protected virtual void SubAction1()
+        {
+            Console.WriteLine("Base::SubAction1");
+        }
+        
+        protected virtual void SubAction2()
+        {
+            Console.WriteLine("Base::SubAction2");
+        }
+        
+        protected virtual void SubAction3()
+        {
+            Console.WriteLine("Base::SubAction3");
+        }
+        
+        // метод добавился в новой версии класса
+        private void InitialAction()
+        {
+            Console.WriteLine("Base::InitialAction");
+        }
+        
+        // метод добавился в новой версии класса
+        private void FinalAction()
+        {
+            Console.WriteLine("Base::FinalAction");
+        }
+    }
+
+    internal class TemplateMethodDerived : TemplateMethodBase
+    {
+
+        protected override void SubAction1()
+        {
+            Console.WriteLine("Derived::SubAction1");
+        }
+        
+        protected override void SubAction2()
+        {
+            Console.WriteLine("Derived::SubAction2");
+        }
+        
+        protected override void SubAction3()
+        {
+            Console.WriteLine("Derived::SubAction3");
+        }
+
+
+    }
+}
---
+++ CySharpAdvancedCourse/Lesson11/L11Task1/Program.cs
@@ -0,0 +1,104 @@
+﻿namespace L11Task1
+{
+    /*
+    Задание 2 
+    Создайте консольное приложение, которое в различных потоках сможет получить доступ к 2-м файлам. 
+    Считайте из этих файлов содержимое и попытайтесь записать полученную информацию в третий файл. 
+    Чтение/запись должны осуществляться одновременно в каждом из дочерних потоков. 
+    Используйте блокировку потоков для того, чтобы добиться корректной записи в конечный файл.
+    */
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            string filesDir = Directory.GetCurrentDirectory().Split(@"\bin")[0] + "\\files";
+            Console.WriteLine("Путь до файлов: {0}", filesDir);
+
+            FileReader fileReader1 = new FileReader($"{filesDir}\\source_1.txt");
+            FileReader fileReader2 = new FileReader($"{filesDir}\\source_2.txt");
+            
+            FileWriter fileWriter = new FileWriter($"{filesDir}\\destination.txt");
+            
+            fileReader1.ReadFromFileInBackground(fileWriter.WriteToFileSynchronously);
+            fileReader2.ReadFromFileInBackground(fileWriter.WriteToFileSynchronously);
+        }
+    }
+
+    internal class FileReader
+    {
+        
+        internal delegate void ProcessFileRead(byte[] fileContent);
+
+        private readonly string _filePath;
+        
+        internal FileReader(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        
+        internal void ReadFromFileInBackground(ProcessFileRead callback)
+        {
+            Thread thread = new Thread(CallbackWrapper);
+            Console.WriteLine($"Читаю файл в потоке {thread.GetHashCode()}");
+            thread.Start();
+            
+            return;
+
+            void CallbackWrapper() => ReadFromFile(callback);
+        }
+
+        internal void ReadFromFile(ProcessFileRead callback)
+        {
+            FileInfo file = new FileInfo(_filePath);
+
+            byte[] buffer;
+                
+            using (FileStream fileStream = file.OpenRead())
+            {
+                buffer = new byte[fileStream.Length];
+                
+                fileStream.Read(buffer, 0, buffer.Length);
+            }
+
+            callback.Invoke(buffer);
+        }
+    }
+    
+    internal class FileWriter
+    {
+        
+        private static readonly object Block = new object();
+        
+        private readonly string _filePath;
+        
+        internal FileWriter(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        internal void WriteToFileSynchronously (byte[] fileNewContent)
+        {
+            lock (Block) // если закомментировать, то получим ошибку доступа к файлу
+            {
+                Console.WriteLine($"Записываю файл в потоке {Thread.CurrentThread.GetHashCode()}");
+
+                FileInfo file = new FileInfo(_filePath);
+                byte[] fileExistentContent;
+                
+                using (FileStream fileStream = file.OpenRead())
+                {
+                    fileExistentContent = new byte[fileStream.Length];
+                
+                    fileStream.Read(fileExistentContent, 0, fileExistentContent.Length);
+                }
+                
+                using (FileStream fileStream = file.OpenWrite())
+                {
+                    byte[] fileMergedContent = fileExistentContent.Concat(fileNewContent).ToArray();
+                    fileStream.Write(fileMergedContent, 0, fileMergedContent.Length);
+                }
+            }
+        }
+    }
+}
